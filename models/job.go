0c603a5ff1a30b9ee75aaf0f12dbfa23ff52b1a1
JC Cormier <D9077C3D19645573@gmail.com>
2024-03-05 13:26:28 -0400

    Update model for job definition

--- models/job.go
+++ models/job.go
@@ -1,11 +1,11 @@
 package models
 
 type Job struct {
-	Id          int    `json:"id"`
-	Delay       int    `json:"delay"`
-	Queue       string `json:"queue"`
-	TeamId      int    `json:"team_id"`
-	UserId      int    `json:"user_id"`
-	Description string `json:"description"`
-	Payload     string `json:"payload"`
+	Id         int    `json:"id"`
+	Delay      int    `json:"delay"`
+	Queue      string `json:"queue"`
+	Class      string `json:"class"`
+	Method     string `json:"method"`
+	RetryCount int    `json:"retry_count"`
+	Data       string `json:"data"`
 }
