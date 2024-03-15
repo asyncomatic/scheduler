d8dcf9a445a7d5ebabffbea648425cab3a32b4b2
JC Cormier <D9077C3D19645573@gmail.com>
2024-03-15 10:17:06 -0300

    Add MIT license

--- queue/kafka/queue_writer.go
+++ queue/kafka/queue_writer.go
@@ -1,3 +1,8 @@
+//  Copyright (c) 2024 JC Cormier
+//  All rights reserved.
+//  SPDX-License-Identifier: MIT
+//  For full license text, see LICENSE file in the repo root or https://opensource.org/licenses/MIT
+
 package kafka
 
 import (
